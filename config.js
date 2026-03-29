fbc5d48d476ac1c477a76f6a7c5c19299e43c2bc
guntoro88dummy <guntoro88@gmail.com>
2026-03-29 19:51:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ const API_KEY = "AIzaSyA4KQC0WZ99jAlqiIpILKH5AOopsYc0yLQ";
 const CHANNEL_ID = "UCSKrztE8VRnE3XxXG3ATduw";
 
 const VIDEO_LIMIT = 6;
-const TRENDING_LIMIT = 4;
+const TRENDING_LIMIT = 5;
